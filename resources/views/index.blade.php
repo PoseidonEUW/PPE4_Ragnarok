b233305f492640363c2ed18416fec41c9b7d6659
root <yoann.brnq@gmail.com>
2020-12-11 17:03:30 +0100

    WEB DEBUT

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<html><head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>test</title>
+</head>
+<body>
+<div class="ladiv1">bonjour paris</div>
+<div class="ladiv2">Vous avez bien répondu</div>
+</body></html>
