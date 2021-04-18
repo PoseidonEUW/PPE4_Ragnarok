2ad62d1e353963c4997f21d42d0f3e5295764368
PoseidonEUW <jessyprovenzal@hotmail.com>
2021-04-18 02:11:13 +0200

    Mes Reservations Fini

--- resources/views/reservations.blade.php
+++ resources/views/reservations.blade.php
@@ -9,20 +9,40 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Verifier tes reservations
+                    <h2 class="font-semibold text-md text-gray-800 leading-tight mb-4">Mes Reservations en Cours :</h2>
                     <!-- Billet -->
-                        <!-- Billet-->
+
+
                     @foreach($reservations as $reservation)
 
-                        <div class="flex mt-4">
+                        <div class="flex ml-24 border-t-2 border-gray-500">
                             <span>Nom de Manif : {{$reservation->LIBELLEMANIF}}</span>
                             <span class="mx-2">| </span>
                             <span>Quantiter : {{$reservation->QUANTITERESERVATION}}</span>
+                            <span class="mx-2">| </span>
+                            <span>Prix : {{$reservation->PRIXMANIF}}</span>
+
                         </div>
+                        <span class="ml-24 font-bold">Prix Total : </span>{{$reservation->PRIXMANIF*$reservation->QUANTITERESERVATION}} €
+
+                        <!-- Billet-->
+
+
+
+
                     @endforeach
 
-            </div>
+
+                </div>
+
         </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-12">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Mes Reservations passé :
+
+                </div>
+
+            </div>
         </div>
     </div>
 
