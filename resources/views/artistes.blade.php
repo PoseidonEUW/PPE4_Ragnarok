83f8a892efaec2d78200e0cccb81412b100bcae9
YoBrnq <yoann.brnq@gmail.com>
2021-04-20 00:15:32 +0200

    Update artistes.blade.php

--- resources/views/artistes.blade.php
+++ resources/views/artistes.blade.php
@@ -36,8 +36,7 @@
 {{--                        </div>--}}
 
 {{--                    </div>--}}
-        
-{{--            Le code que tas fait avant a été commenté, j'ai fait ça pour toi! Bonne continuation --}}
+
                         <div class="flex flex-row mt-6 ml-3">
                             <div class="ImageInfo">
                             <input type="hidden" name="num" value={{$artiste->IDPERSONNE}}>
@@ -45,17 +44,25 @@
                                 <img src="{{asset('images/samantha-gades-fIHozNWfcvs-unsplash.jpg')}}" alt="image" class="w-96" style="width:24rem">
                             </a>
                             </div>
-                            <div class="ml-24">Test</div>
-                        </div>
+                            <div class="ml-24">
+                                <div class=" text-3xl text-md-center uppercase mt-3">{{$artiste->NOMPERSONNE}} {{$artiste->PRENOMPERSONNE}} </div>
+                                <div class=" text-md-center text-red-600  mt-2">Rôle de l'artiste : {{$artiste->ROLEARTISTE}} </div>
+                                <div class="">{{$artiste->LIBELLEARTISTES}}</div>
+                                <div class=" text-md-center  mt-2"><p class="text-red-600 ">Suivre l'artiste : </p>{{$artiste->LIENFBSITEARTISTE}} </div>
+                            </div>
 
 
-                @endforeach
-                    <h2 class=" cursor-default uppercase tracking-wider text-gray-300 text-3xl font-semibold">
-                        Les Intervenants
-                    </h2>
-    </div>
+                            
+                            </div>
 
 
-{{--    </div>--}}
+                    @endforeach
+                        <h2 class=" cursor-default uppercase tracking-wider text-gray-300 text-3xl font-semibold">
+                            Les Intervenants
+                        </h2>
+        </div>
+
+
+    {{--    </div>--}}
 {{--    </div>--}}
 @endsection
