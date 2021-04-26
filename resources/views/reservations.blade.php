c370e06a4afd9d718de7159dee572adb57b13859
YoBrnq <yoann.brnq@gmail.com>
2021-04-26 10:39:21 +0200

    LARAVEL TRI RESERVATION

--- resources/views/reservations.blade.php
+++ resources/views/reservations.blade.php
@@ -10,11 +10,9 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h2 class="font-semibold text-md text-gray-800 leading-tight mb-4">Mes Reservations en Cours :</h2>
+                 @foreach($reservations as $reservation)
+                    @if($reservation->DATEMANIF < date("Y-m-d"))
                     <!-- Billet -->
-
-
-                    @foreach($reservations as $reservation)
-
                         <div class="flex ml-24 border-t-2 border-gray-500">
                             <span>Nom de Manif : {{$reservation->LIBELLEMANIF}}</span>
                             <span class="mx-2">| </span>
@@ -33,21 +31,36 @@
 
                         <!-- Billet-->
 
-
-
-
-                    @endforeach
-
-
+                        @endif
+                     @endforeach
                 </div>
 
         </div>
+
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-12">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Mes Reservations passé :
+                    <h2 class="font-semibold text-md text-gray-800 leading-tight mb-4">Mes Reservations passées :</h2>
+                    @foreach($reservations as $reservation)
+                        @if($reservation->DATEMANIF > date("Y-m-d"))
+                    <div class="flex ml-24 border-t-2 border-gray-500">
+                        <span>Nom de Manif : {{$reservation->LIBELLEMANIF}}</span>
+                        <span class="mx-2">| </span>
+                        <span>Quantité : {{$reservation->QUANTITERESERVATION}}</span>
+                        <span class="mx-2">| </span>
+                        <span>Prix : {{$reservation->PRIXMANIF}}</span>
 
+                    </div>
+                    <div class="flex items-center justify-end mt-4">
+
+                        <x-button class="ml-4">
+                            {{ __('Mettre un avis') }}
+                        </x-button>
+                    </div>
+                    <span class="ml-24 font-bold">Prix Total : </span>{{$reservation->PRIXMANIF*$reservation->QUANTITERESERVATION}} €
+
+                        @endif
+                    @endforeach
                 </div>
-
             </div>
         </div>
     </div>
