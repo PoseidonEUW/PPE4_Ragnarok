25ea4f41562319bfb5e41449a9b38715c619b827
PoseidonEUW <jessyprovenzal@hotmail.com>
2021-05-07 01:17:25 +0200

    Website RESPONSIBLE!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width-device-width, initial-scale-1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portofolio</title>
     <link rel="stylesheet" href="/css/app.css">
     <link rel="shortcut icon" href="{{ asset('https://pics.freeicons.io/uploads/icons/png/21201021021574330934-512.png') }}">
